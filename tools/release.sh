fffe7f429866d2bfe0e0384c7f74264990924c51
Dan Rollo <danrollo@gmail.com>
2024-11-21 12:56:30 -0500

    release workaround

--- tools/release.sh
+++ tools/release.sh
@@ -8,9 +8,9 @@ fi
 
 version=v$(go run . --version)
 
-if git tag -l | grep "$version"; then
-  exit
-fi
+#if git tag -l | grep "$version"; then
+#  exit
+#fi
 
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_EMAIL"
